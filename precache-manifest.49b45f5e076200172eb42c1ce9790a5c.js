ebdfb829480f2d967a0ff7a3ee322130e4015fc1
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2020-07-25 17:46:49 -0400

    Updates

---
+++ precache-manifest.49b45f5e076200172eb42c1ce9790a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f00f562c7d2ce73af0a84c39a8bbfe1",
+    "url": "/thewinery/index.html"
+  },
+  {
+    "revision": "8f2bdce3672384edf7da",
+    "url": "/thewinery/static/css/main.3a4f03fd.chunk.css"
+  },
+  {
+    "revision": "4599ed6e10364a20f5db",
+    "url": "/thewinery/static/js/2.ac2af287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thewinery/static/js/2.ac2af287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2bdce3672384edf7da",
+    "url": "/thewinery/static/js/main.9c7f7eb2.chunk.js"
+  },
+  {
+    "revision": "ab5c876b7c2d36b898af",
+    "url": "/thewinery/static/js/runtime-main.18798271.js"
+  }
+]);
